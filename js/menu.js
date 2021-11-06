18a0d4e85b898d36df3d7243b0169e0af0cbab0f
Vladeek <vlad.berinchikoff@gmail.com>
2021-11-06 19:52:08 +0300

    Add output to console in restaurant.html

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 const foodBand = 'https://fooddelievery-93b4b-default-rtdb.firebaseio.com/db/food-band';
 
-const renderItems = () => {
-   console.log(data);
+const renderItems = (data) => {
+   data.forEach(element => {
+      console.log(element);
+   });
 }
 
 fetch(`${foodBand}.json`)
-   .then((response) => { response.json; })
-   .then((data) => { renderItems(data); })
-   .catch((error) => { console.log.error });
+   .then((response) => response.json())
+   .then((data) => renderItems(data))
+   .catch((error) => console.log.error);
