9c532cbc7d26b54c4fdc58dd366d7cd7708345a3
Vladeek <vlad.berinchikoff@gmail.com>
2021-11-07 21:00:30 +0300

    Complete third task

--- js/auth.js
+++ js/auth.js
@@ -1,63 +1,63 @@
-const buttonAuth = document.querySelector('.button-auth');
-const buttonCloseAuth = document.querySelector('.close-auth');
-const buttonOut = document.querySelector('.button-out');
-const modalAuth = document.querySelector('.modal-auth');
-const userName = document.querySelector('.user-name');
-const logInForm = document.getElementById('logInForm');
-const inputLogin = document.getElementById('login');
-const inputPassword = document.getElementById('password');
-const errorMesageLogin = document.querySelector('.error-message')
+const buttonAuth = document.querySelector(".button-auth");
+const buttonCloseAuth = document.querySelector(".close-auth");
+const buttonOut = document.querySelector(".button-out");
+const modalAuth = document.querySelector(".modal-auth");
+const userName = document.querySelector(".user-name");
+const logInForm = document.getElementById("logInForm");
+const inputLogin = document.getElementById("login");
+const inputPassword = document.getElementById("password");
+const errorMesageLogin = document.querySelector(".error-message");
 
 const login = (user) => {
-   buttonAuth.style.display = 'none';
-   buttonOut.style.display = 'flex';
+  buttonAuth.style.display = "none";
+  buttonOut.style.display = "flex";
 
-   userName.style.display = 'flex';
-   userName.textContent = user.login;
+  userName.style.display = "flex";
+  userName.textContent = user.login;
 
-   modalAuth.style.display = 'none';
-}
+  modalAuth.style.display = "none";
+  //return true;
+};
 
 const logout = (user) => {
-   buttonAuth.style.display = 'flex';
-   buttonOut.style.display = 'none';
+  buttonAuth.style.display = "flex";
+  buttonOut.style.display = "none";
 
-   userName.style.display = 'none';
-   userName.textContent = '';
+  userName.style.display = "none";
+  userName.textContent = "";
 
-   localStorage.removeItem('user');
-}
+  localStorage.removeItem("user");
+  //return false;
+};
 
-buttonAuth.addEventListener('click', () => {
-   modalAuth.style.display = 'flex';
-})
+buttonAuth.addEventListener("click", () => {
+  modalAuth.style.display = "flex";
+});
 
-buttonOut.addEventListener('click', () => {
-   logout();
-})
+buttonOut.addEventListener("click", () => {
+  logout();
+});
 
-buttonCloseAuth.addEventListener('click', () => {
-   modalAuth.style.display = 'none';
-   errorMesageLogin.style.visibility = 'hidden';
-})
+buttonCloseAuth.addEventListener("click", () => {
+  modalAuth.style.display = "none";
+  errorMesageLogin.style.visibility = "hidden";
+});
 
-logInForm.addEventListener('submit', (event) => {
-   event.preventDefault();
-   const user = {
-      login: inputLogin.value,
-      password: inputPassword.value
-   }
-   if (inputLogin.value == '') {
-      errorMesageLogin.style.visibility = 'visible';
-      errorMesageLogin.textContent = 'Введите логин!';
-   }
-   else {
-      localStorage.setItem('user', JSON.stringify(user));
-      login(user);
-   }
+logInForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const user = {
+    login: inputLogin.value,
+    password: inputPassword.value,
+  };
+  if (inputLogin.value == "") {
+    errorMesageLogin.style.visibility = "visible";
+    errorMesageLogin.textContent = "Введите логин!";
+  } else {
+    localStorage.setItem("user", JSON.stringify(user));
+    login(user);
+  }
+});
 
-})
-
-if (localStorage.getItem('user')) {
-   login(JSON.parse(localStorage.getItem('user')));
+if (localStorage.getItem("user")) {
+  login(JSON.parse(localStorage.getItem("user")));
 }
--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,61 @@
-const foodBand = 'https://fooddelievery-93b4b-default-rtdb.firebaseio.com/db/food-band';
+const cardsMenu = document.querySelector(".cards-menu");
+
+const changeTitle = (restaurant) => {
+  const restaurantTitle = document.querySelector(".restaurant-title");
+  restaurantTitle.textContent = restaurant.name;
+};
+
+const changeRate = (restaurant) => {
+  const restaurantRate = document.querySelector(".rating");
+  restaurantRate.textContent = restaurant.stars;
+};
+
+const changePrice = (restaurant) => {
+  const restaurantPrice = document.querySelector(".price");
+  restaurantPrice.textContent = "От " + restaurant.price + " ₽";
+};
+
+const changeKitchen = (restaurant) => {
+  const restaurantKitchen = document.querySelector(".category");
+  restaurantKitchen.textContent = restaurant.kitchen;
+};
 
 const renderItems = (data) => {
-   data.forEach(element => {
-      console.log(element);
-   });
-}
+  data.forEach(({ description, id, image, name, price }) => {
+    const card = document.createElement("div");
 
-fetch(`${foodBand}.json`)
-   .then((response) => response.json())
-   .then((data) => renderItems(data))
-   .catch((error) => console.log.error);
+    card.classList.add("card");
+    card.innerHTML = `<img src="${image}" alt="${name}" class="card-image" />
+    <div class="card-text">
+       <div class="card-heading">
+          <h3 class="card-title card-title-reg">${name}</h3>
+       </div>
+       <div class="card-info">
+          <div class="ingredients">${description}
+          </div>
+       </div>
+       <div class="card-buttons">
+          <button class="button button-primary button-add-cart">
+             <span class="button-card-text">В корзину</span>
+             <span class="button-cart-svg"></span>
+          </button>
+          <strong class="card-price-bold">${price} ₽</strong>
+       </div>
+    </div>`;
+    cardsMenu.append(card);
+  });
+};
+
+if (localStorage.getItem("restaurant")) {
+  const restaurant = JSON.parse(localStorage.getItem("restaurant"));
+  changeTitle(restaurant);
+  changeRate(restaurant);
+  changePrice(restaurant);
+  changeKitchen(restaurant);
+  fetch(`./db/${restaurant.products}`)
+    .then((response) => response.json())
+    .then((data) => renderItems(data))
+    .catch((error) => console.log.error);
+} else {
+  window.location.href = "/";
+}
